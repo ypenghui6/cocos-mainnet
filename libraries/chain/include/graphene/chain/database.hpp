ecbdc3bad37f034ec3b63cd95fd859569a6ed919
ypenghui6 <ypenghui6@163.com>
2019-12-24 22:31:18 +0800

    fix compile error

--- libraries/chain/include/graphene/chain/database.hpp
+++ libraries/chain/include/graphene/chain/database.hpp
@@ -69,6 +69,7 @@ class database : public db::object_database
     //////////////////// db_management.cpp ////////////////////
 
     database(const fc::path& data_dir);
+    database(const fc::path& data_dir, const boost::program_options::variables_map &options);
     ~database();
 
     enum validation_steps
@@ -212,7 +213,7 @@ class database : public db::object_database
     void set_concerned_candidates(const flat_set<vote_id_type> candidates ){concerned_candidates=candidates; }
     void set_message_cache_size_limit(uint16_t message_cache_size_limit);
     void set_deduce_in_verification_mode(bool flag){deduce_in_verification_mode=flag;}
-    void set_option(const boost::program_options::variables_map &options);
+//     void set_option(const boost::program_options::variables_map &options);
 
     // 执行定时任务
     fc::signal<void(const uint32_t participating, bool maybe_allow_transaction)> allowe_continue_transaction;
