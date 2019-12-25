292c4d9637072892faf8721a491a02fc47dca261
ypenghui6 <ypenghui6@163.com>
2019-12-25 13:38:53 +0800

    fix get options

--- programs/witness_node/main.cpp
+++ programs/witness_node/main.cpp
@@ -249,8 +249,10 @@ int main(int argc, char** argv)
       bpo::notify(options);
       
       auto _options = &options;
-      wlog("------------------------------------------------------- ${test}",("test", _options->at("max-order-his-seconds-per-market").as<uint64_t>()));
-      wlog("------------------------------------------------------- ${test}",("test", _options->at("contract_private_data_size").as<uint64_t>()));
+      auto max_market = _options->at("max-order-his-seconds-per-market").as<uint64_t>();
+      auto contract_private_data_size = _options->at("contract_private_data_size").as<uint64_t>();
+      wlog("------------------------------------------------------- ${test}",("test", max_market));
+      wlog("------------------------------------------------------- ${test}",("test", contract_private_data_size));
 
       node->initialize_db(data_dir, options);
 
