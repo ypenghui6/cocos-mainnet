e9b9fcb79ccf7a9ab6c18a1b62d0714ccfb1bbea
ypenghui6 <ypenghui6@163.com>
2020-02-10 14:54:42 +0800

    fix error

--- libraries/chain/vesting_balance_evaluator.cpp
+++ libraries/chain/vesting_balance_evaluator.cpp
@@ -151,11 +151,11 @@ asset vesting_balance_withdraw_evaluator::calculate_fee( const operation& op, co
    auto extensions = db().current_parameters_extensions();
    auto base_value = vesting_balance_withdraw_default_fee;
    if(extensions.size() > 0 ){
-      base_value = db().current_parameters_extensions()[0];
+      base_value = uint64_t(db().current_parameters_extensions()[0]);
    }
 
    //auto base_value = op.visit( calc_fee_visitor( *this, op ) ); //  calc_fee_visitor 依次 调用 fee_schedule -> fee_helper ->  Operation::fee_parameters_type
-   auto scaled = fc::uint128(base_value) * scale;   // 比例
+   auto scaled = fc::uint128(base_value) * GRAPHENE_100_PERCENT;   // 比例
    scaled /= GRAPHENE_100_PERCENT;
    FC_ASSERT( scaled <= GRAPHENE_MAX_SHARE_SUPPLY );
    //idump( (base_value)(scaled)(core_exchange_rate) );
