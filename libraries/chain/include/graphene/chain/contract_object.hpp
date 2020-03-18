ed77f40b7a335452182ff8ee8e54fe89e07d5e52
ypenghui6 <ypenghui6@163.com>
2020-03-18 17:19:53 +0800

    fix bugs

--- libraries/chain/include/graphene/chain/contract_object.hpp
+++ libraries/chain/include/graphene/chain/contract_object.hpp
@@ -42,7 +42,7 @@ class contract_object : public graphene::db::abstract_object<contract_object>
     lua_map contract_data;
     lua_map contract_ABI;
     contract_bin_code_id_type lua_code_b_id;
-    public_key_rsa_type random_key;
+    key_rsa random_key;
 
   public:
     contract_object(){user_invoke_share_percent = 100;};
