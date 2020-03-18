e303266a72882299ea6b2792a3db60161a60ef76
ypenghui6 <ypenghui6@163.com>
2020-03-18 13:11:45 +0800

    fix bugs

--- libraries/chain/protocol/types.cpp
+++ libraries/chain/protocol/types.cpp
@@ -42,7 +42,7 @@ namespace graphene { namespace chain {
     public_key_rsa_type::public_key_rsa_type( const fc::public_key& pubkey )
     {
        const fc::bytes& pub = pubkey.serialize();
-       key_data(pub);
+       key_data(pub.data());
     };
 
     public_key_rsa_type::public_key_rsa_type( const std::string& base58str )
